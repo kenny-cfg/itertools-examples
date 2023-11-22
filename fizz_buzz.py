95ed8beed6577aa8da5340d7a3035a4ef8eb5fcd
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-22 20:08:27 +0000

    First four items are correct

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,2 +1,7 @@
+import itertools
+
+
 def get_fizz_buzz_iterator():
-    pass
+    positive_integers = itertools.count(start=1, step=1)
+    iterator = (f"{x}" for x in positive_integers)
+    return iterator
--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -1,3 +1,4 @@
+import itertools
 import unittest
 
 from fizz_buzz import get_fizz_buzz_iterator
@@ -6,6 +7,8 @@ from fizz_buzz import get_fizz_buzz_iterator
 class FizzBuzzTest(unittest.TestCase):
     def test_first_four_numbers(self):
         iterator = get_fizz_buzz_iterator()
+        first_four_items = list(itertools.islice(iterator, 0, 4))
+        self.assertEqual(first_four_items, ["1", "2", "3", "4"])
 
 
 if __name__ == '__main__':
